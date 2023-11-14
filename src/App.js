33d20fc0e02fb3256d2c61c8ce37bbd7fecf5c8b
ramakrishnahm <ramakrishna729m@gmail.com>
2023-11-14 11:06:07 +0530

    modified app component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import Slider from './slider-01/Slider'
-import Tabs from './tabs-02/Tabs'
+
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Slider/> */}
-      <Tabs/>
+      <Slider/>
+     
      
     </div>
   );

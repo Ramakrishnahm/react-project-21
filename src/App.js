d0753dd0ce8f8f5ff19d7651b5144482cac179fc
ramakrishnahm <ramakrishna729m@gmail.com>
2023-11-14 09:19:36 +0530

    added tabs project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-import Slider from './slider/Slider'
+import Slider from './slider-01/Slider'
+import Tabs from './tabs-02/Tabs'
 
 
 function App() {
   return (
     <div className="App">
-      <Slider/>
+      {/* <Slider/> */}
+      <Tabs/>
      
     </div>
   );
